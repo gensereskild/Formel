0097618b50bba576693fdcbd3d429a9c591ca720
Leanderk03 <lk.kongsrud@gmail.com>
2022-10-13 19:44:36 +0200

    megaPush

--- Classes.js
+++ Classes.js
@@ -1,14 +1,38 @@
 
+
+class match {
+    constructor(number_of_waves, difficulty) {
+        this._number_of_waves = number_of_waves;
+        this._difficulty = difficulty;
+    }
+
+    next_wave() {
+        
+    }
+
+}
+
 class wave {
-    constructor(wave_number, red_number, blue_number, green_number) {
-        this._wave_number = wave_number; //trenger kanskej ikke waveNumber
-        this._rednumber = red_number;
+    constructor(red_number, blue_number, green_number) {
+        this._red_number = red_number;
         this._blue_number = blue_number;
         this._green_number = green_number;
 
         this._reds = [];
         this._blues = []; //lister for å holde hvert ballong objekt.
         this._greens = [];
+
+
+        for(b = 0; b < this._red_number; b++) {
+            this._reds.push(Balloon("red", 1)) //konstruerer røde blåe og grønne
+        }
+        for(b = 0; b < this._blue_number; b++) {
+            this._blues.push(Balloon("blue", 1))
+        }
+        for(b = 0; b < this._green_number; b++) {
+            this._greens.push(Balloon("green", 1))
+        }
+
     }
     
     draw_wave() {
@@ -17,7 +41,11 @@ class wave {
         }
 
         for (var i = 0; i < this._blues.length; i++) { //draw blåe
-            this._reds[i].draw();
+            this._blues[i].draw();
+        }
+
+        for (var i = 0; i < this._greens.length; i++) { //draw grønne
+            this._greens[i].draw();
         }
     }
 
