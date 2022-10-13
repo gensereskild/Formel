20261e02769ab0133c9242c7f753dbbfe4ec52c8
Leanderk03 <lk.kongsrud@gmail.com>
2022-10-13 19:18:27 +0200

    test

--- Classes.js
+++ Classes.js
@@ -1,8 +1,21 @@
 
-class Balloon {
-    constructor(color, hp)
+class wave {
+    constructor(waveNumber)
+
 
 }
+
+
+class Balloon {
+    constructor(color, hp) {
+        this._color = color;
+        this._hp = hp;
+    }
+
+
+
+
+
 
 class Tower {
     constructor()
