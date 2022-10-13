57016e7d77c42bc8039ef1cdc6f8049d193663d5
Leanderk03 <lk.kongsrud@gmail.com>
2022-10-13 19:28:16 +0200

    Ny Classes

--- Classes.js
+++ Classes.js
@@ -1,7 +1,25 @@
 
 class wave {
-    constructor(waveNumber)
+    constructor(wave_number, red_number, blue_number, green_number) {
+        this._wave_number = wave_number; //trenger kanskej ikke waveNumber
+        this._rednumber = red_number;
+        this._blue_number = blue_number;
+        this._green_number = green_number;
 
+        this._reds = [];
+        this._blues = []; //lister for å holde hvert ballong objekt.
+        this._greens = [];
+    }
+    
+    draw_wave() {
+        for (var i = 0; i < this._reds.length; i++) { //draw røde
+            this._reds[i].draw();
+        }
+
+        for (var i = 0; i < this._blues.length; i++) { //draw blåe
+            this._reds[i].draw();
+        }
+    }
 
 }
 
@@ -12,11 +30,9 @@ class Balloon {
         this._hp = hp;
     }
 
-
-
-
-
-
-class Tower {
-    constructor()
+    draw() {
+        //ESKILD FYLL HER FOR DRAW
+        return
+    }
 }
+
